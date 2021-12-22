93833cb5977fa6312a79bfa220f1858f01ad2759
Elena Myadzeleva <sagva2014@gmail.com>
2021-12-22 22:47:04 +0100

    Passed props to the Modal component

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <h1>{{ title }}</h1>
   <input type="text" ref="name" />
   <button @click="handleClick">Click me</button>
-  <Modal />
+  <Modal :header="header" :text="text" theme="sale" />
 </template>
 
 <script>
@@ -15,6 +15,8 @@ export default {
   data() {
     return {
       title: "Vue app",
+      header: "Congratulations!",
+      text: "You are signed up",
     };
   },
   methods: {
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="backdrop">
-    <div class="modal">
-      <h1>Modal Title</h1>
-      <p>modal content</p>
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ header }}</h1>
+      <p>{{ text }}</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+};
+</script>
 
 <style>
 .modal {
@@ -25,5 +31,13 @@
 
 .modal h1 {
   color: #fdcdfd;
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+.modal.sale h1 {
+  color: white;
 }
 </style>
